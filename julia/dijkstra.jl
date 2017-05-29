b01057771ded8c2c78787ec0c9f2b9a206434925
Alexander Sanchez <asanchez.dev@gmail.com>
2017-05-29 14:52:17 -0400

    pushing the full path to dijkstra

--- julia/dijkstra.jl
+++ julia/dijkstra.jl
@@ -1,10 +1,21 @@
-const GRAPH = 
+const SAMPLE_GRAPH = 
   [1 2 1 4 5;
    3 1 3 3 4;
    0 1 0 2 2;
    3 0 3 1 2;
    0 0 0 0 0]
 
+const SAMPLE_LARGE_GRAPH = 
+  [16 25 20 18 25 4 28 22 14 8;
+   28 21 4 19 8 2 30 9 15 9;
+   21 8 19 13 27 22 29 15 28 16;
+   25 6 22 12 1 21 30 17 15 26;
+   25 19 20 14 14 15 13 8 16 4;
+   11 2 28 16 8 13 1 10 26 8;
+   18 28 23 17 15 29 5 20 30 10;
+   8 7 1 21 15 12 28 26 9 18;
+   14 3 3 29 24 12 12 20 8 8;
+   10 5 20 1 6 8 20 9 17 24]
 
 """
 # dijkstra
@@ -18,11 +29,11 @@ function dijkstra(graph, source)
   q = Set([])
 
   dist = Vector(n)
-  prev = Vector(n)
+  prev = Vector{Vector{Int}}(n)
 
   for vi in 1:n
     dist[vi] = 9_999_999
-    prev[vi] = nothing
+    prev[vi] = []
     push!(q, vi)
   end
 
@@ -36,7 +47,7 @@ function dijkstra(graph, source)
       alt = dist[u] + graph[u, v]
       if alt < dist[v]
         dist[v] = alt
-        prev[v] = u
+        push!(prev[v], u)
       end
     end
   end
