b8853069a17a8ce929ad299e27ef44fa2877b2fa
Alexander Sanchez <asanchez.dev@gmail.com>
2017-06-03 20:02:18 -0400

    I don't think I'm allowed to use slices in this way

--- julia/mergesort.jl
+++ julia/mergesort.jl
@@ -1,2 +1,35 @@
-function merge(lst)
+function merge(a, b)
+  res = []
+  while !isempty(a) && !isempty(b)
+    if a[1] <= b[1]
+      push!(res, a[1])
+      a = a[2:end]
+    else
+      push!(res, b[1])
+      b = b[2:end]
+    end
+  end
+
+  if !isempty(a)
+    res = [res ; a]
+  end
+  if !isempty(b)
+    res = [res ; b]
+  end
+
+  res
+end
+
+function merge_sort(lst)
+  n = length(lst)
+  n <= 1 && return lst
+  m = Int(floor(n/2))
+
+  left = lst[1:m]
+  right = [m:n]
+
+  left = merge_sort(left)
+  right = merge_sort(right)
+
+  merge(left, right)
 end
